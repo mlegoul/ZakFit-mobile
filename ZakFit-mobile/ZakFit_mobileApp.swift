8382c0ed0b9be2b46efbe1e2f0bac84e15148191
Mehdi <legoullonmehdi@gmail.com>
2025-12-03 16:58:40 +0100

    Initial commit

---
+++ ZakFit-mobile/ZakFit_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFit_mobileApp.swift
+//  ZakFit-mobile
+//
+//  Created by Mehdi Legoullon on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZakFit_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
