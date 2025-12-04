c2aa0d81caac755c153d3f919408513e74a15bef
Mehdi <legoullonmehdi@gmail.com>
2025-12-04 17:42:41 +0100

    Add colors and ini Seach view

--- ZakFit-mobile/Features/Search/SearchView.swift
+++ ZakFit-mobile/Features/Search/SearchView.swift
@@ -9,11 +9,113 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        Text("Résultats de recherche")
-            .navigationTitle("Recherche")
+        
+        Text("Recherche")
+            .font(.custom("Lexend-Medium", size: 24))
+            .padding()
+        
+        VStack(alignment: .leading, spacing: 20) {
+
+            
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Catégories")
+                    .font(.custom("Lexend-Medium", size: 20))
+                    .padding()
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color("light-grey"))
+                    .frame(width: 360, height: 250)
+                    .overlay(
+                        VStack(alignment: .leading, spacing: 0) {
+                            HStack(spacing: 0) {
+                                Image(systemName: "flame.fill")
+                                    .foregroundColor(Color("light-red"))
+                                    .padding(8)
+                                
+                                Text("Activités")
+                                    .font(.custom("Lexend-Medium", size: 16))
+                                    .padding(.leading, 8)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 16)
+                            }
+                            .padding(.vertical, 12)
+                            .padding(.leading, 16)
+                            
+                            Divider()
+                                .padding(.leading, 44)
+                            
+                            HStack(spacing: 0) {
+                                Image(systemName: "fork.knife")
+                                    .foregroundColor(Color("light-green"))
+                                    .padding(8)
+                                
+                                Text("Repas")
+                                    .font(.custom("Lexend-Medium", size: 16))
+                                    .padding(.leading, 8)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 16)
+                            }
+                            .padding(.vertical, 12)
+                            .padding(.leading, 16)
+                            
+                            Divider()
+                                .padding(.leading, 44)
+                            
+                            HStack(spacing: 0) {
+                                Image(systemName: "calendar")
+                                    .foregroundColor(Color("light-purple"))
+                                    .padding(8)
+                                
+                                Text("Historique")
+                                    .font(.custom("Lexend-Medium", size: 16))
+                                    .padding(.leading, 8)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 16)
+                            }
+                            .padding(.vertical, 12)
+                            .padding(.leading, 16)
+                            
+                            Divider()
+                                .padding(.leading, 44)
+                            
+                            HStack(spacing: 0) {
+                                Image(systemName: "doc.fill")
+                                    .foregroundColor(Color("light-blue"))
+                                    .padding(8)
+                                
+                                Text("Rapports")
+                                    .font(.custom("Lexend-Medium", size: 16))
+                                    .padding(.leading, 8)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 16)
+                            }
+                            .padding(.vertical, 12)
+                            .padding(.leading, 16)
+                        }
+                    )
+                    .padding(.horizontal)
+            }
+            Spacer()
+        }
+        .padding(.top, 40)
     }
 }
-
 
 #Preview {
     SearchView()
